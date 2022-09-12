7640f92a68616222e3f26fca8a18ea15b5cacf7b
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-13 05:16:58 +0900

    implement pelemay_backend as a delegating backend

--- test/pelemay_backend_test.exs
+++ test/pelemay_backend_test.exs
@@ -1,4 +1,18 @@
 defmodule PelemayBackendTest do
   use ExUnit.Case
   doctest PelemayBackend
+
+  setup do
+    Nx.default_backend(PelemayBackend)
+    :ok
+  end
+
+  @unrelated_doctests [
+    default_backend: 1
+  ]
+
+  doctest Nx,
+    except:
+      [:moduledoc] ++
+        @unrelated_doctests
 end
