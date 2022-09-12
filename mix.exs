c2fbcbfe1793619bcba28d81631eb93247998d2b
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-13 05:46:26 +0900

    install and set up ex_doc

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule PelemayBackend.MixProject do
   use Mix.Project
 
+  @version "0.1.0-dev"
+  @source_url "https://github.com/zeam-vm/pelemay_backend"
+
   def project do
     [
       app: :pelemay_backend,
-      version: "0.1.0-dev",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -24,7 +28,63 @@ defmodule PelemayBackend.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:nx, "~> 0.3.0"}
+      {:nx, "~> 0.3.0"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
+
+  defp docs do
+    [
+      main: "PelemayBackend",
+      logo: "Pelemay.png",
+      before_closing_body_tag: &before_closing_body_tag/1,
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ],
+      source_ref: "v#{@version}",
+      source_url: @source_url
+    ]
+  end
+
+  defp before_closing_body_tag(:html) do
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css" integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.js" integrity="sha384-FaFLTlohFghEIZkw6VGwmf9ISTubWAVYW8tG8+w2LAIftJEULZABrF9PPFv+tVkH" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/contrib/auto-render.min.js" integrity="sha384-bHBqxz8fokvgoJ/sc17HODNxa42TlaEhB+w8ZJXTc2nZf1VgEaFZeZvT4Mznfz0v" crossorigin="anonymous"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        renderMathInElement(document.body, {
+          delimiters: [
+            {left: '$$', right: '$$', display: true},
+            {left: '$', right: '$', display: false}
+          ]
+        });
+      });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
+    <script>mermaid.initialize({startOnLoad: true})</script>
+    <script src="https://cdn.jsdelivr.net/npm/vega@5.20.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.1.1"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.18.2"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        for (const codeEl of document.querySelectorAll("pre code.vega-lite")) {
+          try {
+            const preEl = codeEl.parentElement;
+            const spec = JSON.parse(codeEl.textContent);
+            const plotEl = document.createElement("div");
+            preEl.insertAdjacentElement("afterend", plotEl);
+            vegaEmbed(plotEl, spec);
+            preEl.remove();
+          } catch (error) {
+            console.log("Failed to render Vega-Lite plot: " + error)
+          }
+        }
+      });
+    </script>
+    """
+  end
+
+  defp before_closing_body_tag(_), do: ""
 end
