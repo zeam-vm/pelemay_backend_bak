f2d9a6a3c39b92ce38c894588671483729a922b1
Susumu Yamazaki <zacky1972@gmail.com>
2023-01-20 10:00:41 +0900

    fix failed doctests of Nx.weighted_mean/3

--- test/pelemay_backend/backend_test.exs
+++ test/pelemay_backend/backend_test.exs
@@ -221,8 +221,9 @@ defmodule PelemayBackend.BackendTest do
     # window_product: 3,
     # window_reduce: 5,
     window_scatter_max: 5,
-    window_scatter_min: 5
+    window_scatter_min: 5,
     # window_sum: 3
+    weighted_mean: 3
   ]
 
   doctest Nx,
