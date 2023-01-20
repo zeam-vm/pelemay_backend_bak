0c615482fffc1573c00971e61bf2ad3e2a5e929e
Susumu Yamazaki <zacky1972@gmail.com>
2023-01-20 09:48:51 +0900

    upgrade Nx to 0.4.0

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule PelemayBackend.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:nx, "~> 0.3.0"},
+      {:nx, "~> 0.4.0"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:openblas_builder, "~> 0.1.0-dev", github: "zeam-vm/openblas_builder", branch: "main"},
       {:elixir_make, "~> 0.6", runtime: false},
